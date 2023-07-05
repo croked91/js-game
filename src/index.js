13aac0425612d1ffae0693de8b4d36e3182bd86d
Егор <etikhomirov@1tv.com>
2023-07-05 10:49:52 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import img from './assets/logo.png'
+import './index.scss'
+
+console.log('###INIT###')
+console.log(img)
+
+const logo = document.querySelector('.logo')
+logo.style.backgroundImage = `url(${img})`
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    assetModuleFilename: 'assets/[name][ext]'
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+    poll: 1000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[hash][ext]'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'), 
+    })
+  ],
+  devServer: {
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+    hot: true 
+  }
+}
